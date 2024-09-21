e76fe9ff2d338cb55a256bf5d736dc049b1719bb
xietianshu <xts513614@163.com>
2024-09-21 16:17:32 +0800

    1

--- source/extensions/omni.isaac.lab/omni/isaac/lab/utils/assets.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/utils/assets.py
@@ -21,7 +21,8 @@ from typing import Literal
 import carb
 import omni.client
 
-NUCLEUS_ASSET_ROOT_DIR = carb.settings.get_settings().get("/persistent/isaac/asset_root/cloud")
+# NUCLEUS_ASSET_ROOT_DIR = carb.settings.get_settings().get("/persistent/isaac/asset_root/cloud")
+NUCLEUS_ASSET_ROOT_DIR = "omniverse://localhost/NVIDIA/Assets/Isaac/4.1/"
 """Path to the root directory on the Nucleus Server."""
 
 NVIDIA_NUCLEUS_DIR = f"{NUCLEUS_ASSET_ROOT_DIR}/NVIDIA"
