6e7b14681de03641a81d85352e15c85c2c91c66b
xietianshu <xts513614@163.com>
2024-09-21 21:55:44 +0800

    1

--- run2.sh
+++ run2.sh
@@ -1,3 +1,2 @@
 conda activate isaaclab
-python /home/xts/.local/share/ov/pkg/isaac-sim-4.1.0/IsaacLab/source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-Flat-Cassie-Play-v0/
---max iteration 99 --num env 10 --seed 40 --headless
+python /home/xts/.local/share/ov/pkg/isaac-sim-4.1.0/IsaacLab/source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-Flat-Cassie-Play-v0 --max iteration 99 --num env 10 --seed 40 --headless
